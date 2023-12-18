318c9d362f1244f575a76c446e90ae234f50153f
Maria <mariadiananaghiu09@gmail.com>
2023-12-18 09:29:50 +0100

    add/last changes react

--- servidor/client/ClassyFinance/src/components/Footer/Nav/Nav.jsx
+++ servidor/client/ClassyFinance/src/components/Footer/Nav/Nav.jsx
@@ -3,15 +3,15 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 const Nav = () => {
   return (
-    <nav>
-  <ul className="nav-bar">
-    <li><Link to='/'><img src="../../../public/assets/home.png" alt="Home" /></Link></li>
-    <li><Link to='/profile'><img src="../../../public/assets/user.png" alt="User space" /></Link></li>
-    <li><Link to='/categorieslist'><img src="../../../public/assets/categorieslist.png" alt="Categories" /></Link></li>
-    <li><Link to='/addexpense'><img src="../../../public/assets/addexpense.png" alt="Add expense" /></Link></li>
-    
-  </ul>
-</nav>
+    <nav id="navContainer">
+      <ul>
+        <li className="nav-bar-item"><Link to='/'><img src="../../../public/assets/home.png" alt="Home" /></Link></li>
+        <li className="nav-bar-item"><Link to='/profile'><img src="../../../public/assets/user.png" alt="User space" /></Link></li>
+        <li className="nav-bar-item"><Link to='/categorieslist'><img src="../../../public/assets/categorieslist.png" alt="Categories" /></Link></li>
+        <li className="nav-bar-item"><Link to='/addexpense'><img src="../../../public/assets/addexpense.png" alt="Add expense" /></Link></li>
+
+      </ul>
+    </nav>
 
   );
 };
